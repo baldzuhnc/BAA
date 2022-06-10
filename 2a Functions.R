592ee8aedecad4ebc722b6e1fd9b666f58a70a05
cu780nfaa <cu780nfaa@rstudio01-new.sc.uni-leipzig.de>
2022-06-10 14:13:32 +0200

    changes

--- 2a Functions.R
+++ 2a Functions.R
@@ -83,12 +83,64 @@ classify_industry <- function(cleantibble){
   cleantibble_key <- merge(cleantibble, baa_key, by = "Beruf", all.x = T)
   
   cleantibble_key_isco <- left_join(cleantibble_key, isco_baa, by = "baa_key") %>%
-    mutate(creative = map_int(isco_key, ~any(str_starts(., creative_industries))))
-  
+    mutate(creative = map_int(isco_key, ~any(str_starts(., creative_industries,))),
+           occupation_major = case_when(str_starts(isco_key, "1") ~ "Managers",
+                                        str_starts(isco_key, "2") ~ "Professionals",
+                                        str_starts(isco_key, "3") ~ "Technicians and Associate Professionals",
+                                        str_starts(isco_key, "4") ~ "Clerical Support Workers",
+                                        str_starts(isco_key, "5") ~ "Services and Sales Workers",
+                                        str_starts(isco_key, "6") ~ "Skilled Agricultural, Forestry and Fishery Workers",
+                                        str_starts(isco_key, "7") ~ "Craft and Related Trades Workers",
+                                        str_starts(isco_key, "8") ~ "Plant and Machine Operators And Assemblers",
+                                        str_starts(isco_key, "9") ~ "Elementary Occupations",
+                                        str_starts(isco_key, "0") ~ "Armed Forces Occupations"),
+           occupation_submajor = case_when(str_starts(isco_key, "11") ~ "Chief Executives, Senior Officials and Legislators",
+                                           str_starts(isco_key, "12") ~ "Administrative and Commercial Managers",
+                                           str_starts(isco_key, "13") ~ "Production and Specialized Services Managers",
+                                           str_starts(isco_key, "14") ~ "Hospitality, Retail and Other Services Managers",
+                                           str_starts(isco_key, "21") ~ "Science and Engineering Professionals",
+                                           str_starts(isco_key, "22") ~ "Health Professionals",
+                                           str_starts(isco_key, "23") ~ "Teaching Professionals",
+                                           str_starts(isco_key, "24") ~ "Business and Administration Professionals",
+                                           str_starts(isco_key, "25") ~ "Information and Communications Technology Professionals",
+                                           str_starts(isco_key, "26") ~ "Legal, Social and Cultural Professionals",
+                                           str_starts(isco_key, "31") ~ "Science and Engineering Associate Professionals",
+                                           str_starts(isco_key, "32") ~ "Health Associate Professionals",
+                                           str_starts(isco_key, "33") ~ "Business and Administration Associate Professionals",
+                                           str_starts(isco_key, "34") ~ "Legal, Social, Cultural and Related Associate Professionals",
+                                           str_starts(isco_key, "35") ~ "Information and Communications Technicians",
+                                           str_starts(isco_key, "41") ~ "General and Keyboard Clerks",
+                                           str_starts(isco_key, "42") ~ "Customer Services Clerks",
+                                           str_starts(isco_key, "43") ~ "Numerical and Material Recording Clerks",
+                                           str_starts(isco_key, "44") ~ "Other Clerical Support Workers",
+                                           str_starts(isco_key, "51") ~ "Personal Services Workers",
+                                           str_starts(isco_key, "52") ~ "Sales Workers",
+                                           str_starts(isco_key, "53") ~ "Personal Care Workers",
+                                           str_starts(isco_key, "54") ~ "Protective Services Workers",
+                                           str_starts(isco_key, "61") ~ "Market-oriented Skilled Agricultural Workers",
+                                           str_starts(isco_key, "62") ~ "Market-oriented Skilled Forestry, Fishery and Hunting Workers",
+                                           str_starts(isco_key, "63") ~ "Subsistence Farmers, Fishers, Hunters and Gatherers",
+                                           str_starts(isco_key, "71") ~ "Building and Related Trades Workers (excluding Electricians)",
+                                           str_starts(isco_key, "72") ~ "Metal, Machinery and Related Trades Workers",
+                                           str_starts(isco_key, "73") ~ "Handicraft and Printing Workers",
+                                           str_starts(isco_key, "74") ~ "Electrical and Electronic Trades Workers",
+                                           str_starts(isco_key, "75") ~ "Food Processing, Woodworking, Garment and Other Craft and Related Trades Workers",
+                                           str_starts(isco_key, "81") ~ "Stationary Plant and Machine Operators",
+                                           str_starts(isco_key, "82") ~ "Assemblers",
+                                           str_starts(isco_key, "83") ~ "Drivers and Mobile Plant Operators",
+                                           str_starts(isco_key, "91") ~ "Cleaners and Helpers",
+                                           str_starts(isco_key, "92") ~ "Agricultural, Forestry and Fishery Labourers",
+                                           str_starts(isco_key, "93") ~ "Labourers in Mining, Construction, Manufacturing and Transport",
+                                           str_starts(isco_key, "94") ~ "Food Preparation Assistants",
+                                           str_starts(isco_key, "95") ~ "Street and Related Sales and Services Workers",
+                                           str_starts(isco_key, "96") ~ "Refuse Workers and Other Elementary Workers",
+                                           str_starts(isco_key, "01") ~ "Commissioned Armed Forces Officers",
+                                           str_starts(isco_key, "02") ~ "Non-commissioned Armed Forces Officers",
+                                           str_starts(isco_key, "03") ~ "Armed Forces Occupations, Other Ranks")
+    )
   
   return(cleantibble_key_isco)
 }
-
 
 ############## Ordnet jeder Anzeige die Arbeitsmarktregion (AMR) zu, in die sie fällt ##############
 
--- 2b clean, annotate, save.R
+++ 2b clean, annotate, save.R
@@ -4,7 +4,7 @@ source(file = "2a Functions.R") #should be cluster ready with this
 ############## Liste Tibbles Rawdata ##############
 df_list <- list.files(path = "/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/Rawdata/", pattern = "*.rds*", full.names = T) %>% 
   map(function(x){
-    read_rds(x) %>% slice(1:10)
+    read_rds(x)
   })
 
 ############## from df_list create cleaned, mapped, analyzed tibbles for each industry ##############
@@ -18,18 +18,18 @@ map(df_list, function(x){
     classify_industry() %>%
     mapamr() %>%
     add_popamr() %>%
-    textual_complexity_measures() %>%
-    en_ingles()
+    textual_complexity_measures() #%>%
+    #en_ingles()
   
   #ergebnis df pro branche in finaldata abspeichern
   branche <- str_replace_all(branche, "/ ", "")
-  saveRDS(df, file = paste0("/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/1d_1withduplicates", branche, ".rds"))
+  saveRDS(df, file = paste0("/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/1d_1_withduplicates/", branche, ".rds"))
   
 })
 
 
 ############## One dataframe ##############
-df <- list.files(path = "/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/1d_1withduplicates/", 
+df <- list.files(path = "/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/1d_1_withduplicates/", 
                     pattern = "*.rds*", 
                     full.names = T) %>% 
   
@@ -40,11 +40,8 @@ df <- list.files(path = "/home/sc.uni-leipzig.de/cu780nfaa/rstudio01/BAA/Data/1d
   })
 
 
-saveRDS(df, file = paste0("Data/", "final_each_categorized", Sys.time() ,".rds"))
+saveRDS(df, file = paste0("Data/", "final_each_categorized_", Sys.time() ,".rds"))
 
 
-
-
-each <- readRDS("Data/final_each2022-06-03 02:34:19.rds")
 
 
