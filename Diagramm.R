38df64b6181e22bcc0e178ff49772acc7ffe08bc
cu780nfaa <cu780nfaa@rstudio01-new.sc.uni-leipzig.de>
2022-06-14 23:03:49 +0200

    added steps to analysis

--- Diagramm.R
+++ Diagramm.R
@@ -2,26 +2,28 @@ library(DiagrammeR)
 library(DiagrammeRsvg)
 library(rsvg)  # for saving svg
 
-d <- grViz("
+
+# Selection ####
+selection <- grViz("
 digraph {
   compound = true
   graph [ranksep = 0.5]
   
   node [shape = none]
     A [label = 'Macro', shape = none, style = bold]
-    D [label = 'Meso \n Inter-industry effect', shape = none]
-    G [label = 'Micro \n Intra-industry effect', shape = none]
+    D [label = 'Meso\n Inter-industry effect', shape = none]
+    G [label = 'Micro\n Intra-industry effect', shape = none]
     
-    B [label = 'Complex \n economic activites']
-    C [label = 'Spatial \n agglomeration']
-    
-    
-    E [label = 'Industries which produce \n complex goods']
-    F [label = 'Settlement in large \n metropolitan areas']
+    B [label = 'Complex\n economic activites']
+    C [label = 'Spatial\n agglomeration']
     
     
-    H [label = 'Companies need employees \n from manifold backgrounds']
-    I [label = 'Companies settle where \n location factors are beneficial']
+    E [label = 'Industries which produce\n complex goods']
+    F [label = 'Settlement in large\n metropolitan areas']
+    
+    
+    H [label = 'Companies need employees\n from manifold backgrounds']
+    I [label = 'Companies settle where\n location factors are beneficial']
 
     
   edge []
@@ -44,17 +46,64 @@ digraph {
     rankdir = 'TB'
     
     A -> D -> G [style = invis]
-           
-                  
-                  
-                  
     
-}
-")
-d
+}")
+
+selection
 
 
-#Export
+
+# Interconnectivity ####
+interconnectivity <- grViz("
+digraph {
+  compound = true
+  graph [ranksep = 0.5]
+  
+  node [shape = none]
+    A [label = 'Macro', shape = none, style = bold]
+    D [label = 'Meso\n Inter-industry effect', shape = none]
+    G [label = 'Micro\n Intra-industry effect', shape = none]
+     
+    B [label = 'Complex\n economic activites']
+    C [label = 'Spatial\n agglomeration']
+    
+    
+    E [label = 'Industries which produce\n complex goods']
+    F [label = 'Settlement in large\n metropolitan areas']
+    
+    
+    H [label = 'Companies need employees\n from manifold backgrounds']
+    I [label = 'Companies settle where\n location factors are beneficial']
+
+    
+  edge []
+    
+    rankdir = 'LR'
+    B -> C [minlen = 15, style = dashed, dir = both]
+    B -> E [style = dashed]
+    
+    E -> F [minlen = 10, style = dashed, dir = both]
+    E -> H [style = dashed]
+    
+    H -> I [minlen = 5, style = dashed]
+    I -> F [style = dashed, style = dashed]
+    F -> C [style = dashed, style = dashed]
+    
+{rank = same; A; B; C}
+{rank = same; D; E; F}
+{rank = same; G; H; I}
+           
+    rankdir = 'TB'
+    
+    A -> D -> G [style = invis]
+    
+}")
+
+interconnectivity
+
+
+
+# Export ####
 export_svg(d) %>%
   charToRaw %>% 
   rsvg_pdf("model.pdf")
